0a1d83c7b5d8970b89d8555603e32f7bce5a09af
lauga-_t <lauga-_t@DESKTOP-8G07KSI>
2017-12-05 13:31:34 +0100

    loop game

---
+++ pbrain-Gorillaz/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gorillaz
+{
+    class Game
+    {
+        private int[][] _board;
+        private int _xMax;
+        private int _yMax;
+        //private IA _IA;
+        private int _end;
+
+        public void start()
+        {
+            //_IA = new IA();
+            _end = 0;
+
+            while (_end == 0)
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ pbrain-Gorillaz/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Gorillaz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Game game = new Game();
+
+                game.start();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
